dfa52b9f3d59d41f700245c9386b3920c0733ff7
Pranab Sarma <pranavirat12@gmail.com>
2024-12-01 12:15:55 +0530

    add app.py commit

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,16 @@
 from flask import Flask
 
 def create_app():
-    app = Flask(__name__, template_folder='../templates')
-    
-    # Import and register routes
-    from .routes import main
+    """
+    Factory function to create and configure the Flask app.
+    """
+    app = Flask(__name__, template_folder='../templates', static_folder='../static')
+
+    # Register Blueprints
+    from .routes import main  # Import your Blueprint
     app.register_blueprint(main)
-    
+
+    # Additional Configurations (if any)
+    app.config['SECRET_KEY'] = 'your_secret_key_here'  # Example: Add a secret key
+
     return app
